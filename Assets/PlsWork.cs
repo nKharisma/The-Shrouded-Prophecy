a338ae1e4d004a040328d6b0c1d69bde403ba94d
SeNiah McField <seniahwalker@gmail.com>
2025-01-20 15:26:47 -0500

    Testing Changes
    IDK

--- Assets/PlsWork.cs
+++ Assets/PlsWork.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BetterTesting : MonoBehaviour
+public class PlsWork : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
