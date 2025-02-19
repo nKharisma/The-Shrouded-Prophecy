795482dc7e6652f7bd09546945844767b2680da4
SadieNiBu <sadiejklol@gmail.com>
2025-02-18 20:32:57 -0500

    continued work on integrated sc ui
    player stays in bounds

--- Assets/Scripts/Character/SCBattlePlayer/SCBattlePlayerControls.cs
+++ Assets/Scripts/Character/SCBattlePlayer/SCBattlePlayerControls.cs
@@ -5,22 +5,41 @@ using UnityEngine;
 public class SCBattlePlayerControls : MonoBehaviour
 {
     [SerializeField] private float movSpeed;
-    [SerializeField] private RectTransform skillCheckSquare;  // Reference to the square in the canvas
+    [SerializeField] private RectTransform canvas;
+    [SerializeField] private RectTransform skillCheckSquare;
     
     private Vector2 moveInput;
     private Vector2 moveVelocity;
 
     void Update()
     {
-        // Get input for movement (use any method you prefer, such as keyboard input)
         moveInput.x = Input.GetAxisRaw("Horizontal") * movSpeed;
         moveInput.y = Input.GetAxisRaw("Vertical") * movSpeed;
 
-        // Move the square based on the input
         moveVelocity = moveInput.normalized * movSpeed;
 
-        // Update position based on RectTransform (UI element movement)
         skillCheckSquare.anchoredPosition += moveVelocity * Time.deltaTime;
+
+        ClampPanel();
+    }
+
+    private void ClampPanel()
+    {
+        Vector3[] canvasCorners = new Vector3[4];
+        canvas.GetWorldCorners(canvasCorners);
+
+        Vector3[] squareCorners = new Vector3[4];
+        skillCheckSquare.GetWorldCorners(squareCorners);
+
+        float minX = canvasCorners[0].x - squareCorners[0].x + skillCheckSquare.anchoredPosition.x;
+        float maxX = canvasCorners[2].x - squareCorners[2].x + skillCheckSquare.anchoredPosition.x;
+        float minY = canvasCorners[0].y - squareCorners[0].y + skillCheckSquare.anchoredPosition.y;
+        float maxY = canvasCorners[2].y - squareCorners[2].y + skillCheckSquare.anchoredPosition.y;
+
+        float clampedX = Mathf.Clamp(skillCheckSquare.anchoredPosition.x, minX, maxX);
+        float clampedY = Mathf.Clamp(skillCheckSquare.anchoredPosition.y, minY, maxY);
+
+        skillCheckSquare.anchoredPosition = new Vector2(clampedX, clampedY);
     }
 }
 
