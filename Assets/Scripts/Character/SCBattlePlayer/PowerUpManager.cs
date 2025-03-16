ebb53649ddff3d9faa159c2debd91a43c163e70e
SadieNiBu <sadiejklol@gmail.com>
2025-03-15 22:30:29 -0400

    continued to work on powerups
    powerupmanager, border logic

---
+++ Assets/Scripts/Character/SCBattlePlayer/PowerUpManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdatePUSelection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangerPU(int direction)
+    {
+
+    }
+
+    void UpdatePUSelection()
+    {
+
+    }
+
+    void ActivatePU(int index)
+    {
+        switch (index)
+        {
+            case 0:
+
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+        }
+    }
+}
